0aed1a9621547395dff835f33be4eeb726b76376
Aaron Medina <aaron.medina@aligent.com.au>
2024-11-26 11:18:40 +1030

    Redoing the poc using cdkv2

--- next-app/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  output: "standalone", // Required for serverless deployment
-  images: {
-    domains: [], // Replace with valid domains if available, else leave it blank
-  },
-};
-export default nextConfig;
